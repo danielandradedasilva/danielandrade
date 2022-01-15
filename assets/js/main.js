07a59d7100794c9dc6ccec4d04ef86e871af4118
Daniel Andrade <danielandrade539@gmail.com>
2022-01-15 16:02:50 -0300

    criando variáveis no css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,27 @@
 'use strict';
 
+
+
+
 function initAll() {
-
     revealName();
+    effectsOnCards();
+}
 
+function effectsOnCards() {
     VanillaTilt.init(document.querySelectorAll(".ffff"), {
         max: 15,
         speed: 300,
         glare: true,
         "max-glare": 0.3
     });
-
 }
 
 function revealName() {
     document.addEventListener('click', (e) => {
         const el = e.target;
-
         if (el.classList.contains('imgIcons')) {
-            console.log('ok')
+            alert('okkkk')
         }
 
     })
