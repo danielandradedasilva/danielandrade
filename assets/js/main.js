945cf48543140f1063755c9576d9b7508c8f7f01
Daniel Andrade <danielandrade539@gmail.com>
2022-02-27 22:17:18 -0300

    contato finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,30 @@
 'use strict';
 
 function initAll() {
-    effectsOnCards();
-
-}
-
-function effectsOnCards() {
-    VanillaTilt.init(document.querySelectorAll(".fff"), {
-        max: 15,
-        speed: 300,
-        glare: true,
-        "max-glare": 0.3
+    /**Links contatos */
+    document.querySelector('.linkedin').addEventListener('click', () => {
+        window.open('https://www.linkedin.com/in/daniel-andrade-da-silva/');
+    });
+    document.querySelector('.whatsapp').addEventListener('click', () => {
+        window.open('https://web.whatsapp.com/send?phone=5511948307817');
+    });
+    document.querySelector('.github').addEventListener('click', () => {
+        window.open('https://github.com/danielandradedasilva');
     });
 }
 
+/**Scroll Top */
+window.addEventListener('scroll', () => {
+    let scroll = document.querySelector('.scrollTop');
+    scroll.classList.toggle('active', window.scrollY > 950)
+})
 
-
-document.querySelector('.linkedin').addEventListener('click', () => {
-    window.open('https://www.linkedin.com/in/daniel-andrade-da-silva/');
-});
-document.querySelector('.whatsapp').addEventListener('click', () => {
-    window.open('https://web.whatsapp.com/send?phone=5511948307817');
-});
-document.querySelector('.github').addEventListener('click', () => {
-    window.open('https://github.com/danielandradedasilva');
-});
+const scroolTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+}
+document.querySelector('.scrollTop').addEventListener('click', scroolTop);
 
 initAll();
